d1076d492e836b5b5c7adcf90603f631fbf7417c
tg970 <tgoodman@outlook.com>
2018-12-05 20:34:50 -1000

    keypress function. animation example. addstyle example

--- js/app.js
+++ js/app.js
@@ -18,7 +18,16 @@ const changeColor = (event) => {
   $('#steamboat').slideToggle('slow');
 }
 
+function checkKeyPress(e){
+  if (e.keyCode == "39") {
+    console.log('yep');
+    $('#title').addClass( "transform-active" );
+    $('.sub-div').addStyle({'display': 'inline'}) // probs not the right syntax, something similar"
+  }
+}
+
 $(() => {
   let $chip = $('.chip');
-  $chip.on('click', changeColor)
+  $chip.on('click', changeColor);
+  window.addEventListener("keydown", checkKeyPress, false);
 });
